20fcf2ef8904da643fbbccc18d4372dadc697d0d
dominique-saputro <dominique.saputro@gmail.com>
2022-05-29 02:56:23 +0900

    slideshow jQuery
    slideshow still WIP

---
+++ htdocs/js/slideshow.js
@@ -0,0 +1,11 @@
+$("#prev-arrow").click(function(){
+    alert("prev button")
+});
+
+$("#next-arrow").click(function(){
+    alert("next button")
+});
+
+$(".dot").click(function(){
+    alert("dot number: " +$(".dot").index(this))
+});
